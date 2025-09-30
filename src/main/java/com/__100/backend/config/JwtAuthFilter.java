6aae5a3ea9dbc8a5fc9fd35215a5262b13169c6c
aksharkh <aksharkh04@gmail.com>
2025-09-30 16:13:32 +0530

    initiated JwtAuthFilter.java

--- src/main/java/com/__100/backend/config/JwtAuthFilter.java
+++ src/main/java/com/__100/backend/config/JwtAuthFilter.java
@@ -1,9 +1,15 @@
 package com.__100.backend.config;
 
 import com.__100.backend.util.JwtUtil;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
 
 
 @Component
@@ -11,5 +17,9 @@ import org.springframework.web.filter.OncePerRequestFilter;
 public class JwtAuthFilter extends OncePerRequestFilter {
 
     private  final JwtUtil jwtUtil;
-    private final
+
+    protected  void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain ) throws ServletException, IOException{
+
+    }
+
 }
