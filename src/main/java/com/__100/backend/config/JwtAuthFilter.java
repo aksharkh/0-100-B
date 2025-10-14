f7713d36df064db8211acad6337b8363709da057
aksharkh <aksharkh04@gmail.com>
2025-10-14 23:24:51 +0530

    added SecurityConfig and JwtAuthFilter

--- src/main/java/com/__100/backend/config/JwtAuthFilter.java
+++ src/main/java/com/__100/backend/config/JwtAuthFilter.java
@@ -1,11 +1,15 @@
 package com.__100.backend.config;
 
+import com.__100.backend.service.service.CustomUserDetailsService;
 import com.__100.backend.util.JwtUtil;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -17,9 +21,32 @@ import java.io.IOException;
 public class JwtAuthFilter extends OncePerRequestFilter {
 
     private  final JwtUtil jwtUtil;
+    private final CustomUserDetailsService UserDetailsService;
 
+
+    @Override
     protected  void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain ) throws ServletException, IOException{
 
+        final String authHeader = request.getHeader("Authorization");
+
+        String username = null;
+        String jwtToken = null;
+
+        if(authHeader != null && authHeader.startsWith("Bearer ")){
+            jwtToken = authHeader.substring(7);
+            username = jwtUtil.extractUsername(jwtToken);
+        }
+
+        if(username != null && SecurityContextHolder.getContext().getAuthentication() == null){
+            UserDetails userDetails = UserDetailsService.loadUserByUsername(username);
+
+            if(jwtUtil.validateToken(jwtToken, userDetails)){
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
+        }
+        filterChain.doFilter(request, response);
     }
 
 }
--- src/main/java/com/__100/backend/config/SecurityConfig.java
+++ src/main/java/com/__100/backend/config/SecurityConfig.java
@@ -1,12 +1,45 @@
 package com.__100.backend.config;
 
 
+import com.__100.backend.service.service.CustomUserDetailsService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig {
 
+    private final JwtAuthFilter jwtAuthFilter;
+    private final CustomUserDetailsService customUserDetailsService;
 
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth ->auth
+                        .requestMatchers("/auth/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return  new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider(){
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService();
+    }
 }
